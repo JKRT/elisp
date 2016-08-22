efeb1b634d515e6594efa72aea4f21dcaac2d407
John Tinnerholm <johti139@student.liu.se>
2016-08-22 14:28:00 +0200

    Added some files for my linux config , initial commit

---
+++ Linux/keybindings.el
@@ -0,0 +1,5 @@
+					;Global keybindings 
+(global-set-key (kbd "<kp-insert>") 'other-window)
+(global-set-key (kbd "<kp-begin>")  'list-buffers)
+(global-set-key (kbd "<kp-right>" ) 'next-buffer)
+(global-set-key (kbd "<kp-left>" )  'previous-buffer)
