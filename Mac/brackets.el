f0d888b5cb3bc9e93cb2204aebcac81cd38e53e0
John Tinnerholm <jtinnerholm@gmail.com>
2016-09-26 21:24:13 +0200

    Added some utility functions for brackets insertion

--- Mac/brackets.el
+++ Mac/brackets.el
@@ -1,14 +1,5 @@
-;But all backup files in the temp dir.
-(setq backup-directory-alist
-          `((".*" . ,temporary-file-directory)))
-    (setq auto-save-file-name-transforms
-          `((".*" ,temporary-file-directory t)))
 
-(defun kill-all-buffers()
-  "Kill all open buffers"
-  (interactive)
-  (mapc 'kill-buffer (buffer-list)))
-
+;Some utility functions to enclose text in different brackets
 
 (defun insert-double-quotes (&optional arg)
     "Inserts double quotes arround a piece of text"
@@ -30,18 +21,11 @@
 (defun insert-square-brackets (&optional arg)
     "Inserts braces arround a piece of text"
   (interactive "P")
-  (insert-pair arg ?\{  ?\} ))
+  (insert-pair arg ?\[  ?\] ))
 
 
 (defun insert-pointy-brackets (&optional arg)
     "Inserts pointy brackets arround  a piece of text"
   (interactive "P")
   (insert-pair arg ?\<  ?\> ))
-
-
-
-
-
-
-
 
