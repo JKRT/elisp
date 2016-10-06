83c5f3ff83b47162983940a5176bbf9cad1950de
John Tinnerholm <jtinnerholm@gmail.com>
2016-10-06 20:37:46 +0200

    Added rtf mode to extra extensions for GNU emacs

--- Mac/otherSettings.el
+++ Mac/otherSettings.el
@@ -10,34 +10,6 @@
   (mapc 'kill-buffer (buffer-list)))
 
 
-(defun insert-double-quotes (&optional arg)
-    "Inserts double quotes arround a piece of text"
-  (interactive "P")
-  (insert-pair arg ?\"  ?\" ))
-
-
-(defun insert-single-quotes (&optional arg)
-    "Inserts single quotes arround a piece of text"
-  (interactive "P")
-  (insert-pair arg ?\'  ?\' ))
-
-
-(defun insert-braces (&optional arg)
-    "Inserts braces arround a piece of text"
-  (interactive "P")
-  (insert-pair arg ?\{  ?\} ))
-
-(defun insert-square-brackets (&optional arg)
-    "Inserts braces arround a piece of text"
-  (interactive "P")
-  (insert-pair arg ?\{  ?\} ))
-
-
-(defun insert-pointy-brackets (&optional arg)
-    "Inserts pointy brackets arround  a piece of text"
-  (interactive "P")
-  (insert-pair arg ?\<  ?\> ))
-
 
 
 
