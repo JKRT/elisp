dc99d06d8611d7813d2498f1be55807748210f1b
John Tinnerholm <jtinnerholm@gmail.com>
2016-11-14 09:14:26 +0100

    Added some settings and support for ctags

--- Linux/c++.el
+++ Linux/c++.el
@@ -2,3 +2,6 @@
 (add-hook 'c++-mode-hook 'irony-mode)
 (add-hook 'c-mode-hook 'irony-mode)
 (add-hook 'objc-mode-hook 'irony-mode)
+
+;Setting up Ctags
+(setq path-to-ctags "/opt/local/bin/ctags") ;
--- Linux/settings.el
+++ Linux/settings.el
@@ -17,3 +17,11 @@
   (interactive)
   (mapc 'kill-buffer (buffer-list)))
 
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list
+   'package-archives
+   '("melpa" . "http://melpa.org/packages/")
+   t)
+  (package-initialize))
