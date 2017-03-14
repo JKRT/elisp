95bc23d637e41c370f3fdac35c0d0c25d7814b6f
John Tinnerholm <jtinnerholm@gmail.com>
2017-03-14 04:23:47 +0100

    Added some new configs for elisp and python

--- Mac/python.el
+++ Mac/python.el
@@ -3,8 +3,11 @@
 (add-hook 'inferior-python-mode-hook
           (lambda () (local-set-key (kbd "s-<up>") 'comint-previous-input)))
 
-
 (add-hook 'inferior-python-mode-hook
 	  (lambda () (local-set-key (kbd "<s-up>") 'comint-previous-input)))
 (add-hook 'inferior-python-mode-hook
 	  (lambda () (local-set-key (kbd "<s-down>") 'comint-next-input)))
+
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    '(auto-complete-mode)))
