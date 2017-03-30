804b85b360ee22aa58113a837d779c8d6f0737ad
John Tinnerholm <jtinnerholm@gmail.com>
2017-03-30 07:59:37 +0200

    Some keybinds

--- Mac/keybinds.el
+++ Mac/keybinds.el
@@ -1,6 +1,8 @@
-; Some global keybindings
+; Some global keybindings for my mac config
 
 (setq mac-option-key-is-meta nil)
 (setq mac-option-modifier nil)
 
 (global-set-key (kbd "s-\\") 'set-mark-command) 
+(global-set-key [(s backspace )] 'backward-kill-word)
+
---
+++ Mac/termMode.el
@@ -0,0 +1,4 @@
+					;Keybinds for term
+
+
+(line-mode)
