1a8adb3861977df910036419e4da5efbf934a988
John Tinnerholm <jtinnerholm@gmail.com>
2017-03-30 08:28:19 +0200

    Some fancy new stuff

---
+++ General/commands.el
@@ -0,0 +1,11 @@
+
+;For repeating complex commands in a simple way
+(global-set-key [(s shift r)]
+		(lambda () (interactive)
+                                 (eval (car command-history))))
+
+;Repeat the last keyboard command.
+(global-set-key [(s r)]
+		'repeat)
+
+
