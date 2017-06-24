74b511daa7e460923501646c52f887129261adcc
John Tinnerholm <jtinnerholm@Johns-MacBook-Pro.local>
2017-06-24 23:11:05 +0200

    Added a main setupfile for mac. Also finally added a .gitignore file

---
+++ Mac/macConfig.el
@@ -0,0 +1,10 @@
+
+(load-file "./elisp/General/commands.elc")
+
+
+(load-file "./elisp/Mac/melpa.elc")
+(load-file "./elisp/Mac/brackets.elc")
+(load-file "./elisp/Mac/touchPad.elc")
+(load-file "./elisp/Mac/otherSettings.elc)
+(load-file "./elisp/Mac/swift.elc")
+(load-file "./elisp/Mac/c++.elc")
