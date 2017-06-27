0ec7ad967ccd6091ab5d49f76c0d9ccec60637c8
JKRT <jtinnerholm@gmail.com>
2017-06-28 00:34:35 +0200

    Fixed a time configuration bug

--- Mac/macConfig.el
+++ Mac/macConfig.el
@@ -1,22 +1,34 @@
 
 
+;Make sure to recompile the essential emacs libraries.
+(byte-recompile-directory (expand-file-name "~/.emacs.d/elisp/Mac/.") 0)
 
 
-(if ( > 21 (string-to-number
-	    (car
-	     (split-string (nth 3
-				(split-string (current-time-string) " ") )":"))))
-					;if bedtime use a darker theme
-    (load-theme 'material t )
-  
-					;Else sunshine and rainbows
-  (load-theme 'material-light t))
+(defun clocktime()
+  (string-to-number
+	   (car
+	    (split-string (nth 3
+
+			       (split-string (current-time-string) " ") )":"))) )
+
+;Set color theme depending on the time of the day
+(cond
+ ((> 21 (clocktime)) (load-theme 'material t ))
+ ((< 7 (clocktime)) (load-theme 'material t ))
+ ((> 7 (clocktime)) (load-theme 'material-light t )))
+
+
+
+
+
+(tool-bar-mode 0)
+(show-paren-mode t)
 
 
 (load-file "./elisp/General/commands.elc")
 (load-file "./elisp/Mac/melpa.elc")
 (load-file "./elisp/Mac/brackets.elc")
 (load-file "./elisp/Mac/touchPad.elc")
-(load-file "./elisp/Mac/otherSettings.elc)
+(load-file "./elisp/Mac/otherSettings.elc")
 (load-file "./elisp/Mac/swift.elc")
 (load-file "./elisp/Mac/c++.elc")
