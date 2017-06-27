ee020200b60c005616eaf68cd25a7d5e92b66d3d
JKRT <jtinnerholm@gmail.com>
2017-06-27 02:57:13 +0200

    Switch theme depending on the time of the day

--- Mac/macConfig.el
+++ Mac/macConfig.el
@@ -1,7 +1,19 @@
 
+
+
+
+(if ( > 21 (string-to-number
+	    (car
+	     (split-string (nth 3
+				(split-string (current-time-string) " ") )":"))))
+					;if bedtime use a darker theme
+    (load-theme 'material t )
+  
+					;Else sunshine and rainbows
+  (load-theme 'material-light t))
+
+
 (load-file "./elisp/General/commands.elc")
-
-
 (load-file "./elisp/Mac/melpa.elc")
 (load-file "./elisp/Mac/brackets.elc")
 (load-file "./elisp/Mac/touchPad.elc")
