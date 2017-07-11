fac03379be1e7d0eef5d80bffc3dba6850211b56
JKRT <jtinnerholm@gmail.com>
2017-07-12 00:11:12 +0200

    Fix

--- Mac/macConfig.el
+++ Mac/macConfig.el
@@ -4,26 +4,19 @@
 (byte-recompile-directory (expand-file-name "~/.emacs.d/elisp/Mac/.") 0)
 
 
-(defun clocktime()
-  ;Extract the hour from the current time
-  (string-to-number
-	   (car
-	    (split-string (nth 3
-			       (split-string (current-time-string) " ") )":"))) )
 (tool-bar-mode 0)
 (show-paren-mode t)
 ;turn of annoying sound
 (setq ring-bell-function 'ignore)
 
 ;Load other configuration files
-(load-file "./elisp/General/commands.elc")
-(load-file "./elisp/Mac/melpa.elc")
-(load-file "./elisp/Mac/brackets.elc")
-(load-file "./elisp/Mac/touchPad.elc")
-(load-file "./elisp/Mac/otherSettings.elc")
-(load-file "./elisp/Mac/swift.elc")
-(load-file "./elisp/Mac/c++.elc")
-(load-file "./elisp/Mac/themeChange.elc"
+(load-file "./melpa.elc")
+(load-file "./brackets.elc")
+(load-file "./touchPad.elc")
+(load-file "./otherSettings.elc")
+(load-file "./swift.elc")
+(load-file "./c++.elc")
+(load-file "./themeChange.elc")
 
 
 ;Todo add code that initilizes the libraries that I use, if they are not installed
--- Mac/themeChange.el
+++ Mac/themeChange.el
@@ -2,21 +2,25 @@
 					;Not that the material package needs to be installed
 
 
+					;Routine to extract the clocktime
+(defun clocktime()
+					;Extract the hour from the current time
+  (string-to-number
+	   (car
+	    (split-string (nth 3
+			       (split-string (current-time-string) " ") )":"))) )
+
+
 (defun check-if-night()
 					;Returns true if the time is deemed being night.
   (or (> (clocktime) 21) (< (clocktime) 7)))
 
-(defun check-if-day()
-					;Returns true if the time is deemed being day.
-  (or (< (clocktime) 21) (> (clocktime) 7)))
-
-
 (defun theme-change-during-night ()
 					;Set color theme depending on the time of the day
-  (cond (
-	 (check-if-night  (load-theme 'material t))
-	 (check-if-day (load-theme 'material-light t )))))
+  (if  (check-if-night )
+      (load-theme 'material t)
+    (load-theme 'material-light t )))
 
-;Call the function every hour
+					;Call the function every hour
 (run-with-timer 0 3600 'theme-change-during-night)
   
