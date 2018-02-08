4ed5e614b9dc60ae9d0738519f7ad7356ba1a91d
John <jtinnerholm@gmail.com>
2018-02-08 10:02:54 +0100

    Moved tags config to another file

--- Linux/c++.el
+++ Linux/c++.el
@@ -4,12 +4,3 @@
 (add-hook 'c++-mode-hook 'flycheck-mode)
 (add-hook 'c++-mode-hook 'auto-complete-mode)
 
-;Setting up Ctags
-(setq path-to-ctags "/usr/bin/ctags") ;
-
- (defun create-tags (dir-name)
-    "Create tags file."
-    (interactive "DDirectory: ")
-    (shell-command
-     (format "%s -f TAGS -e -R %s" path-to-ctags 
-	     (directory-file-name dir-name))))
