d213dee484f55ddff510b8ba08931f42b58ad035
John Tinnerholm <jtinnerholm@Johns-MacBook-Pro.local>
2018-03-25 17:02:30 +0200

    Added some configs

--- Mac/macConfig.el
+++ Mac/macConfig.el
@@ -10,3 +10,12 @@
 
 					;Load other configuration files
 
+(load-file "~/.emacs.d/elisp/Mac/Ielm.elc")
+(load-file "~/.emacs.d/elisp/Mac/brackets.elc")
+(load-file "~/.emacs.d/elisp/Mac/elisp.elc")
+(load-file "~/.emacs.d/elisp/Mac/keybinds.elc")
+(load-file "~/.emacs.d/elisp/Mac/c++.elc")
+(load-file "~/.emacs.d/elisp/Mac/python.elc")
+(load-file "~/.emacs.d/elisp/Mac/themeChange.elc")
+(load-file "~/.emacs.d/elisp/Mac/melpa.elc")
+
