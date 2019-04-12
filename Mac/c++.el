6b42e78909195a2354fcbd7737db5113b098243c
JKRT <jtinnerholm@gmail.com>
2019-04-12 17:29:56 +0200

    Updated elisp and cpp config

--- Mac/c++.el
+++ Mac/c++.el
@@ -1,11 +1,10 @@
-					; C++ related settings
 ;BSD like indention
 (setq c-default-style "bsd"
-      c-basic-offset 8)
-
+      c-basic-offset 4)
 					;Can be extended here with more keybindings
-(defun my-mac-cpp-config ()
+(defun my-mac-cpp-config()
   (auto-complete-mode)
+  (setq indent-tabs-mode nil)
   (local-set-key (kbd "<f5>") 'compile)
   ;Make sure that flycheck checks according to the C++-11 standard for C++
   ;for both GCC and Clang.
@@ -13,11 +12,20 @@
   (setq flycheck-gcc-language-standard "c++11")
   ;Start rtags for fast navigation (Might fail on antother Macintosh!)
   (rtags-start-process-unless-running)
-  (local-set-key (kbd "M-.") 'rtags-find-symbol-at-point)
-  (flycheck-mode))
+  ;Keys for convenient navigation.
+  (local-set-key (kbd "s-.") 'rtags-find-symbol-at-point)
+  (local-set-key (kbd "s-,") 'rtags-find-references-at-point)
+  (local-set-key (kbd "s-;") 'rtags-find-references-tree-at-point)
+  (local-set-key (kbd "s-r") 'rtags-rename-symbol)
+  (local-set-key (kbd "s-h") 'rtags-print-class-hierarchy))
+
+(defun my-flycheck-c++-rtags-setup ()
+  "Configure flycheck-rtags for better experience."
+  (require 'flycheck-rtags)
+  (flycheck-select-checker 'rtags)
+  (flycheck-mode)
+  (setq-local flycheck-check-syntax-automatically nil)
+  (setq-local flycheck-highlighting-mode nil))
 
 (add-hook 'c++-mode-hook 'my-mac-cpp-config)
-;Load company ironmode, and add other configurations for company-mode.
- (eval-after-load 'company
-   '(add-to-list≥
-     'company-backends '(company-irony-c-headers company-irony company-yasnippet)))
+(add-hook 'c++-mode-hook 'my-flycheck-rtags-setup)
--- Mac/elisp.el
+++ Mac/elisp.el
@@ -1,10 +1,14 @@
-(add-hook 'emacs-lisp-mode-hook
-	  (lambda ()  '(auto-complete-mode)))
-
-
-(add-hook 'emacs-lisp-mode-hook
-	  (lambda ()  '(auto-complete-mode)))
-
-
-(add-hook 'emacs-lisp-mode-hook
-	  (lambda () '(local-set-key (kbd "s-(") 'eval-region)))
+					; Configuration that is used when writing Emacs Lisp
+(defun general-settings-emacs-lisp-mode()
+  (auto-complete-mode)
+  (local-set-key (kbd "s-e") 'eval-region))
+(defun rtags-config-for-emacs-lisp-mode()
+  (rtags-start-process-unless-running)
+  (local-set-key (kbd "s-.") 'rtags-find-symbol-at-point)
+  (local-set-key (kbd "s-,") 'rtags-find-references-at-point)
+  (local-set-key (kbd "s-;") 'rtags-find-references-tree-at-point)
+  (local-set-key (kbd "s-r") 'rtags-rename-symbol)
+  (local-set-key (kbd "s-h") 'rtags-print-class-hierarchy))
+;Boot it up
+(add-hook 'emacs-lisp-mode-hook 'general-settings-emacs-lisp-mode)
+(add-hook 'emacs-lisp-mode-hook 'rtags-config-for-emacs-lisp-mode)
