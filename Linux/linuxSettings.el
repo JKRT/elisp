55a1d965d1214a3bc3d8bfd5f5f4cc9846a7a9c2
johti <johti17@liu.se>
2019-06-26 23:43:29 +0200

    Short config for julia. Updated LinuxSettings.el

--- Linux/linuxSettings.el
+++ Linux/linuxSettings.el
@@ -1,6 +1,6 @@
 ;Recompile all files that where recently changed for fast loading
 (byte-recompile-directory (expand-file-name "~/.emacs.d/elisp/Linux/.") 0)
-
+;Load settings for other modes
 (load-file "~/.emacs.d/elisp/Linux/settings.elc")
 (load-file "~/.emacs.d/elisp/Linux/brackets.elc")
 (load-file "~/.emacs.d/elisp/Linux/c++.elc")
@@ -8,16 +8,16 @@
 (load-file "~/.emacs.d/elisp/Linux/melpa.elc")
 (load-file "~/.emacs.d/elisp/Linux/tags.elc")
 (load-file "~/.emacs.d/elisp/Linux/eglotSettings.elc")
-
-;;Setting up Modelica mode
+(load-file "~/.emacs.d/elisp/Linux/susan-mode/susanMode.elc")
 (load-file "~/.emacs.d/elisp/Linux/modelica-mode/modelica-mode.elc")
+;;Setting up Modelica mode
 (autoload 'modelica-mode "modelica-mode" "Modelica Editing Mode" t)
 (setq auto-mode-alist (cons '("\.mo$" . modelica-mode) auto-mode-alist))
 ;Modelica Mode for .mos files.
 (setq auto-mode-alist (cons '("\.mos$" . modelica-mode) auto-mode-alist))
-
 ;;Loading doremi
 (load-file "~/.emacs.d/elisp/Linux/doremi.el/doremi.elc")
 ;;Turn of tabs
 (setq-default indent-tabs-mode nil)
+;Please be quiet emacs :) 
 (setq visible-bell t)
